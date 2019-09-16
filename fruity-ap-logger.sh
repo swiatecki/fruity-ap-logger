2b2c3231150aac87ccfde1b45c75040120620655
Nicholas Swiatecki <nicholas@swiatecki.com>
2019-09-16 15:55:36 +0200

    getting there

--- fruity-ap-logger.sh
+++ fruity-ap-logger.sh
@@ -2,11 +2,13 @@
 
 # By Nicholas Swiatecki, <nswiatec@cisco.com>
 
+BASEDIR="/home/pi/Fruity-AP-logger"
+
 isScreenRunning=$( screen -list | grep -i AP | wc -l)
 
 if [ $isScreenRunning -eq 1 ]
 then
 	echo "Screen running.. ignoring"
 else
-	screen -c ./screen.conf -dmSL 'AP' screen /dev/ttyUSB0
+	screen -c $BASEDIR/screen.conf -dmSL 'AP' /dev/ttyUSB0
 fi
--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-apt-get install screen
+sudo apt-get install screen
+
+sudo cp fruity-ap-logger-startup.sh /etc/init.d/
+sudo chmod +x /etc/init.d/fruity-ap-logger-startup.sh
+sudo update-rc.d fruity-ap-logger-startup.sh defaults
+
+echo "Updating rc.local - hang tight"
+
+# Update rc.local
+sudo sed -i -e '$i sudo /bin/bash /home/pi/Fruity-AP-logger/fruity-ap-logger.sh' /etc/rc.local
+
+echo "Now reboot please.."
